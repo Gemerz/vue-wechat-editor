bf65109499983a2e3c467ed9eea5d20ad3110a82
Gemer <gemer@wizmacau.com>
2016-03-21 17:29:35 +0800

    update

--- src/components/preview.vue
+++ src/components/preview.vue
@@ -3,27 +3,59 @@
     <div class='wechat-editor-wrap'>
       <div class='wechat-editor'>
         <div class='editor'>
-          <div class='editor-item'>
-            <div class='cover'>
-              <img class='cover-img'/>
-              <span class='thumbnail-holder'></span>
-              <h4 class='title'></h4>
-              <div class='editor-item-edit'>
-                <a data-tip data-for='icon-preview'>
-                  <mdl-tooltip for="pv-edit-button">浏览</mdl-tooltip>
-                  <i id="pv-edit-button" class="material-icons">photo</i>
-                </a>
-                <a data-tip data-for='icon-edit' href='javascript:void(0)'>
-                  <mdl-tooltip for="pv-add-button">新增</mdl-tooltip>
-                  <i id="pv-add-button" class="material-icons">add</i>
-                </a>
-                <a data-tip data-for='icon-delete' href='javascript:void(0)'>
-                  <mdl-tooltip for="pv-delete-button">删除</mdl-tooltip>
-                  <i id="pv-delete-button" class="material-icons">delete</i>
-                </a>
+          <div class='editor-item' v-for="preview in items">
+            <div v-if="preview.data.type =='cover'">
+              <div class='cover'>
+                <img class='cover-img'/>
+                <span class='thumbnail-holder'></span>
+                <h4 class='title'></h4>
+                <div class='editor-item-edit'>
+                  <a data-tip data-for='icon-preview'>
+                    <mdl-tooltip for="pv-edit-button">浏览</mdl-tooltip>
+                    <i id="pv-edit-button" class="material-icons">photo</i>
+                  </a>
+                  <a data-tip data-for='icon-edit' href='javascript:void(0)'>
+                    <mdl-tooltip for="pv-add-button">新增</mdl-tooltip>
+                    <i id="pv-add-button" class="material-icons">add</i>
+                  </a>
+                  <a data-tip data-for='icon-delete' href='javascript:void(0)'>
+                    <mdl-tooltip for="pv-delete-button">删除</mdl-tooltip>
+                    <i id="pv-delete-button" class="material-icons">delete</i>
+                  </a>
+                  <div class="sort-pannel">
+                    <a>
+                      <i class="up material-icons">keyboard_arrow_up</i>
+                    </a>
+                    <a>
+                      <i class=" down material-icons">keyboard_arrow_down</i>
+                    </a>
+                  </div>
 
+                </div>
               </div>
             </div>
+            <div v-else>
+              <div class='editor-item'>
+              <span class='thumbnail'>
+              </span>
+                <div class='editor-item-edit'>
+                  <a>
+                    <mdl-tooltip for='@preview.key-pv-edit-button'>浏览</mdl-tooltip>
+                    <i id="{{preview.key}}-pv-edit-button" class="material-icons">photo</i>
+                  </a>
+                  <a>
+                    <mdl-tooltip for="{{preview.key}}pv-add-button">新增</mdl-tooltip>
+                    <i id="{{preview.key}}pv-add-button" class="material-icons">add</i>
+                  </a>
+                  <a>
+                    <mdl-tooltip for="{{preview.key}}pv-delete-button">删除</mdl-tooltip>
+                    <i id="{{preview.key}}pv-delete-button" class="material-icons">delete</i>
+                  </a>
+
+                </div>
+              </div>
+            </div>
+
           </div>
           <div class="editor-add">
             <a class="editor-add-btn" @click="addPreview">
@@ -31,29 +63,7 @@
               <i id="editor-add-button" class="material-icons">add</i>
             </a>
           </div>
-          <!--<div class='editor-item'>-->
-          <!--<span class='thumbnail'>-->
-          <!--</span>-->
-          <!--<div class='editor-item-edit'>-->
-          <!--<a data-tip data-for='icon-preview'><i class='fa fa-eye'></i></a>-->
-          <!--<a data-tip data-for='icon-edit' href='javascript:void(0)'>-->
-          <!--<i class='fa fa-pencil'></i></a>-->
-          <!--<a data-tip data-for='icon-delete' href='javascript:void(0)'>-->
-          <!--<i class='fa fa-trash'></i></a>-->
-          <!--<div class=''>-->
 
-          <!--</div>-->
-          <!--<div id='icon-preview' type='warning' effect='solid'>-->
-          <!--<span>瀏覽</span>-->
-          <!--</div>-->
-          <!--<div id='icon-edit' type='warning' effect='solid'>-->
-          <!--<span>編輯</span>-->
-          <!--</div>-->
-          <!--<div id='icon-delete' type='warning' effect='solid'>-->
-          <!--<span>刪改</span>-->
-          <!--</div>-->
-          <!--</div>-->
-          <!--</div>-->
         </div>
       </div>
     </div>
@@ -63,44 +73,60 @@
 
 <script>
   import {MdlTooltip, MdlButton, MdlTextfield, MdlIconToggle} from 'vue-mdl'
-  //  import store from 'store'
-  import previewStorage from '../store'
-  //  let inititem = {
-  //    key: 0, data: {
-  //      type: 'cover',
-  //      title: '',
-  //      cover: '',
-  //      content: ''
-  //    }
-  //  }
+  import store from 'store'
+  let initItem = [{
+    key: 0, data: {
+      type: 'cover',
+      title: '',
+      cover: '',
+      content: ''
+    }
+  }]
+  store.set('previewST', initItem)
+  //  const previewST = store.get('previewST')
   export default {
     data () {
       return {
-        preview: previewStorage.fetch,
+        previews: store.get('previewST'),
         newPreview: '',
         editedPreview: null,
-        tempKey: 1
+        tempKey: 1,
+        items: initItem
       }
     },
     components: {
       MdlTooltip,
       MdlButton,
       MdlTextfield,
-      MdlIconToggle,
-      "vue-html-editor": require("vue-html-editor")
+      MdlIconToggle
     },
     watch: {
-      preivew: {
-        deep: true,
-        handler: previewStorage.save
+      previews: {
+        handler: function (previews) {
+          store.get('previewST', previews)
+        }
+      },
+      items: {
+        handler: function (items) {
+          store.set('previewST', items)
+        },
+        deep: true
       }
     },
     methods: {
       addPreview: function () {
-//        this.preview.push({title: 'dddddd', completed: false})
-        this.newPreview = ''
+        let newPreview = Object.assign({}, initItem[this.tempKey - 1], {
+          key: this.tempKey++, data: {
+            type: 'list',
+            title: '',
+            cover: '',
+            content: ''
+          }
+        })
+        this.items.push(newPreview)
+        this.previews = store.get('previewST')
+        console.log(this.previews)
       }
-
     }
   }
 </script>
